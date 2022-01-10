f943c51a2743578ea52cc44853e194558d78d86f
Rel <89156264+Rel-ativity@users.noreply.github.com>
2022-01-09 21:05:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Calculate time difference ( hours/minutes only )
-
-
 import datetime
 if __name__ == '__main__':
     while True:
@@ -8,28 +5,12 @@ if __name__ == '__main__':
         now = datetime.datetime.now()
         my_datetime = datetime.datetime.strptime(my_time_string, "%H:%M")
         my_datetime = now.replace(hour=my_datetime.time().hour, minute=my_datetime.time().minute)
-        
-       #This is not the right method to convert time, but i didnt had access to documentation or whatever so.. :)
+
+        # This is not the right method to convert time, but i didnt had access to documentation or whatever so.. :)
         bor = now - my_datetime
         bor = str(bor)
         bor = bor.replace(":00", "")
         hours, minutes = bor.split(":")
         minute = (int(hours) * 60) + int(minutes)
 
-
-
-        #Ive done this for my job, just modify those to fit your needs
-        
-        if minute < 15:
-            print("Gratuit")
-        elif minute <= 60:
-            print("5$")
-        elif minute <= 120:
-            print("8$")
-        elif minute <= 180:
-            print("11$")
-        elif minute <= 239:
-            print("14$")
-        elif minute >= 240:
-            print("17$")
-
+        print(str(minute) + " Minutes Difference")
